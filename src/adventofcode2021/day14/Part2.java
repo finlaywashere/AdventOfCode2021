449ef780acec441b2767bd1407e745b83303e2ba
finlaywashere <finman292004@protonmail.com>
2021-12-14 08:49:39 -0500

    Added wrong, but less wrong day 14 p2

--- src/adventofcode2021/day14/Part2.java
+++ src/adventofcode2021/day14/Part2.java
@@ -4,7 +4,9 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Part2 {
@@ -19,45 +21,59 @@ public class Part2 {
 			pairs.add(split);
 		}
 		in.close();
-		for(int round = 0; round < 10; round++) {
-			String curr = "";
-			int last = 0;
-			for(int i = 0; i < start.length(); i++) {
-				for(int pair = 0; pair < pairs.size(); pair++) {
-					String s1 = pairs.get(pair)[0];
-					String s2 = pairs.get(pair)[1];
-					
-					if(start.substring(i).startsWith(s1)) {
-						curr += start.substring(last, i+1);
-						curr += s2;
-						last = i+1;
+		Map<String,Long> data = new HashMap<String,Long>();
+		for(int i = 0; i < start.length()-1; i++) {
+			String s = start.substring(i, i+2);
+			if(data.containsKey(s))
+				data.put(s, data.get(s)+1);
+			else
+				data.put(s, 1l);
+		}
+		for(int round = 0; round < 40; round++) {
+			Map<String,Long> tmp = new HashMap<String,Long>();
+			for(String s : data.keySet()) {
+				boolean found = false;
+				for(String[] split : pairs) {
+					String s1 = split[0];
+					String s2 = split[1];
+					if(s.equals(s1)) {
+						char[] sC = s.toCharArray();
+						char[] s1C = s2.toCharArray();
+						long value = data.get(s)+1;
+						value += (tmp.containsKey(s) ? tmp.get(s) : 0);
+						tmp.put(""+sC[0]+s1C[0], value);
+						tmp.put(""+s1C[0]+sC[1], value);
+						
+						found = true;
+						break;
 					}
 				}
+				if(!found) {
+					tmp.put(s, data.get(s));
+				}
 			}
-			curr += start.substring(last);
-			start = curr;
+			data = tmp;
 		}
-		int[] counts = new int[26];
-		for(int i = 0; i < 26; i++) {
-			counts[i] = count((char)(i+'A'), start);
+		long[] counts = new long[26];
+		for(String s : data.keySet()) {
+			char[] c = s.toCharArray();
+			counts[c[0]-'A'] += data.get(s);
+			counts[c[1]-'A'] += data.get(s);
 		}
+		char last = start.toCharArray()[start.length()-1];
+		for(int i = 0; i < counts.length; i++) {
+			counts[i] /= 2;
+		}
+		counts[last-'A']--;
 		Arrays.sort(counts);
-		int lowest = 0;
-		for(int i : counts) {
+		long lowest = 0;
+		for(long i : counts) {
 			if(i != 0) {
 				lowest = i;
 				break;
 			}
 		}
-		int answer = counts[counts.length-1]-lowest;
+		long answer = counts[counts.length-1]-lowest;
 		System.out.println("Answer: "+answer);
-	}
-	private static int count(char c, String s) {
-		int count = 0;
-		for(char c1 : s.toCharArray()) {
-			if(c == c1)
-				count++;
-		}
-		return count;
 	}
 }
